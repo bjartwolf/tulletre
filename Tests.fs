55388eccb850d81de995955cd196947927b3fc19
Bjorn Einar Bjarntes <bjartnes@gmail.com>
2021-05-16 18:36:23 +0200

    init

--- Tests.fs
+++ Tests.fs
@@ -9,8 +9,8 @@ type Tree =
   | Empty 
   | Node of Tree * int * Tree
 
-let root =
-  [ 1 .. 10000 ]
+let largeTree =
+  [ 1 .. 30 ]
   |> List.fold (fun (t: Tree) i -> Node(t, i, t) ) Empty 
 
  
@@ -19,18 +19,25 @@ let rec flip (t: Tree) : Tree =
         | Empty -> Empty
         | Node (t1,num,t2) -> Node (flip t2, num, flip t1)
 
-let rec sum (t: Tree) : int =
+let rec sum (t: Tree) : int64 =
     match t with 
-        | Empty -> 0 
-        | Node (t1,num,t2) -> num + sum(t1) + sum(t2) 
+        | Empty -> 0L
+        | Node (t1,num,t2) -> int64(num) + sum(t1) + sum(t2) 
 
+let rec nodes (t: Tree) : int64 =
+    match t with 
+        | Empty -> 0L 
+        | Node (t1,num,t2) -> 1L + nodes(t1) + nodes(t2) 
 
 let tre = Node (Node (Empty,3, Node(Empty, 3, Empty)), 2, Empty)
 
 
-
 [<Fact>]
 let ``My test`` () =
-    printfn "%A" (sum root) 
-    let doubleflip = flip (flip tre)
-    Assert.Equal(tre, doubleflip)
+//    printfn "%A" largeTree 
+    printfn "%A" (nodes largeTree) 
+    let doubleflip = flip (flip largeTree)
+    printfn "ok"
+    printfn "%A" (sum largeTree) 
+    printfn "ok2"
+    Assert.Equal(largeTree, doubleflip)
